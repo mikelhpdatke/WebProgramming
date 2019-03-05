057053f8207b5831806dfbfd3a655a7a3abe7ba2
Luong Duc Tuan Dat <luongductuandat.psa@gmail.com>
2019-03-05 18:01:20 +0700

    demo1

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,11 +1,59 @@
 import React from 'react';
+import { List, Avatar, Tag } from 'antd';
+import hammerLogo from '@/assets/hammer.jpg';
+import Link from 'umi/link';
+import styles from './globalStyle.less';
 
-export default () => (
-  <p style={{ textAlign: 'center' }}>
-    想要添加更多页面？请参考{' '}
-    <a href="https://umijs.org/guide/block.html" target="_blank" rel="noopener noreferrer">
-      umi 区块
-    </a>
-    。
-  </p>
+const data = [
+  {
+    title: 'CHƯƠNG I',
+    subTitle: 'QUY ĐỊNH CHUNG',
+    link: 'chuong1/1',
+  },
+  {
+    title: 'CHƯƠNG II',
+    subTitle: 'CHỨC NĂNG, NHIỆM VỤ VÀ QUYỀN HẠN CỦA CÔNG AN NHÂN DÂN',
+    link: 'chuong2/15',
+  },
+  {
+    title: 'CHƯƠNG III',
+    subTitle: 'TỔ CHỨC CỦA CÔNG AN NHÂN DÂN',
+    link: 'chuong3/17',
+  },
+  {
+    title: 'CHƯƠNG IV',
+    subTitle: 'SĨ QUAN, HẠ SĨ QUAN, CHIẾN SĨ CÔNG AN NHÂN DÂN',
+    link: 'chuong4/20',
+  },
+  {
+    title: 'CHƯƠNG V',
+    subTitle: 'BẢO ĐẢM ĐIỀU KIỆN HOẠT ĐỘNG VÀ CHẾ ĐỘ, CHÍNH SÁCH ĐỐI VỚI CÔNG AN NHÂN DÂN',
+    link: 'chuong5/33',
+  },
+  {
+    title: 'CHƯƠNG VI',
+    subTitle: 'KHEN THƯỞNG VÀ XỬ LÝ VI PHẠM',
+    link: 'chuong6/43',
+  },
+];
+
+export default props => (
+  <div className={styles.root}>
+    <p className={styles.title}>LUẬT CÔNG AN NHÂN DÂN</p>
+
+    <List
+      bordered
+      itemLayout="horizontal"
+      dataSource={data}
+      renderItem={item => (
+        <List.Item>
+          <List.Item.Meta
+            avatar={<Avatar src={hammerLogo} />}
+            title={<Link to={item.link}>{item.title}</Link>}
+            description={item.subTitle}
+          />
+        </List.Item>
+      )}
+    />
+  </div>
 );
